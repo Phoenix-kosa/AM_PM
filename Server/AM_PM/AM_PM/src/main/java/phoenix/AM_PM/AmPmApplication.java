42ca7add0ae2ac3f7c0dcfaaa083881eb7f47f9f
leesl123 <elegarden89@gmail.com>
2023-11-29 09:26:36 +0900

    Create Server, Client 폴더

---
+++ Server/AM_PM/AM_PM/src/main/java/phoenix/AM_PM/AmPmApplication.java
@@ -0,0 +1,13 @@
+package phoenix.AM_PM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmPmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmPmApplication.class, args);
+	}
+
+}
---
+++ Server/AM_PM/AM_PM/src/test/java/phoenix/AM_PM/AmPmApplicationTests.java
@@ -0,0 +1,13 @@
+package phoenix.AM_PM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmPmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
