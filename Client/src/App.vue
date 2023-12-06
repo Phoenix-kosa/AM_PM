811d4270a857faeca6bcc246bc4fec164f5ada21
insiu <insu1124@gmail.com>
2023-12-06 17:33:31 +0900

    excel 시트

--- Client/src/App.vue
+++ Client/src/App.vue
@@ -1,47 +1,53 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <v-app>
+    <v-navigation-drawer v-model="drawer" :rail="rail" permanent>
+      <v-list-item>
+        <v-list-item-avatar>
+          <img src="https://randomuser.me/api/portraits/men/85.jpg">
+        </v-list-item-avatar>
+        <div>John Leider</div> 
+        <v-list-item-action>
+          <v-btn icon @click.stop="rail = !rail">
+            <v-icon>mdi-chevron-left</v-icon>
+          </v-btn>
+        </v-list-item-action>
+      </v-list-item>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
+      <v-divider></v-divider>
 
-  <main>
-    <TheWelcome />
-  </main>
+      <v-list dense nav>
+        <v-list-item v-for="menu in menus" :key="menu.title" :to="menu.to">
+          <v-list-item-icon>
+            <v-icon>{{ menu.icon }}</v-icon>
+          </v-list-item-icon>
+          <div>{{ menu.title }}</div>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-main>
+      <router-view></router-view>
+    </v-main>
+  </v-app>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
+<script setup>
+import { ref } from 'vue';
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
+const drawer = ref(true);
+const rail = ref(false);
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+const menus = ref([
+  {
+    title: '홈',
+    icon: 'mdi-home',
+    to: '/home',
+  },
+  {
+    title: '엑셀',
+    icon: 'mdi-file-excel',
+    to: '/excel',
+  },
+]);
+</script>
--- Server/src/main/java/phoenix/AM_PM/AmPmApplication.java
+++ Server/src/main/java/phoenix/AM_PM/AmPmApplication.java
@@ -2,12 +2,16 @@ package phoenix.AM_PM;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 
 @SpringBootApplication
 public class AmPmApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(AmPmApplication.class, args);
 	}
-
+	@Bean
+	public ServerEndpointExporter serverEndpointExporter(){
+		return new ServerEndpointExporter();
+	}
 }
