16e72e26a67145b73454d086ae8c5525fbe672ff
hanseungwan1 <seungwanhan542@gmail.com>
2023-12-13 18:30:59 +0900

    feat: 마이페이지 개발

--- Client/src/pages/MyPage.vue
+++ Client/src/pages/MyPage.vue
@@ -2,8 +2,8 @@
   <div class="mypage_container">
     <div class="mypage_wrapper">
       <img src="../assets/images/mainPage/santa_flying.png" alt="" />
-      <button class="profile_container">프로필 이미지 변경</button>
-      <form action="post" class="form_container">
+      <button class="profile_btn">프로필 이미지 변경</button>
+      <form @submit.prevent="submitForm" class="form_container">
         <div class="input_container">
           <label>닉네임</label>
           <input type="text" />
@@ -20,12 +20,28 @@
           <label>비밀번호 확인</label>
           <input type="password" />
         </div>
+        <button class="edit_btn">수정</button>
       </form>
     </div>
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { onMounted } from "vue";
+import { myInfoReq } from "../api/common";
+
+onMounted(() => {
+  console.log("Awdw");
+});
+
+const submitForm = () => {
+  myInfoReq()
+    .then((res) => {
+      console.log("성공");
+    })
+    .catch((error) => {});
+};
+</script>
 
 <style scoped>
 @import "../assets/css/mypage.css";
