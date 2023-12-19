35508abf3c763a8af24b035d7c9fa3171520d93d
hanseungwan1 <seungwanhan542@gmail.com>
2023-12-19 11:20:49 +0900

    feat: 헤더 사용자명 추가 , 토큰 만료 예외 처리

--- Client/src/components/Header.vue
+++ Client/src/components/Header.vue
@@ -7,7 +7,7 @@
         alt="main_logo"
       />
       <div class="login_container">
-        <p>노인수 님</p>
+        <p>{{ name }}</p>
         <router-link to="/mypage">
           <p>MyPage</p>
         </router-link>
@@ -17,7 +17,30 @@
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { getMyInfoReq } from "../api/common";
+import { expireToken } from "../api/config";
+import { ref, onMounted } from "vue";
+
+const name = ref("");
+onMounted(() => {
+  getName();
+});
+
+const getName = () => {
+  getMyInfoReq()
+    .then((res) => {
+      return res.data;
+    })
+    .then((data) => {
+      name.value = data.nickname;
+      console.log(data);
+    })
+    .catch((err) => {
+      expireToken(err, getName);
+    });
+};
+</script>
 <style scoped>
 @import "../assets/css/header.css";
 </style>
