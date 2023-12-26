b3d43d37c586d24e6ffa2a622b7f7278e852bcdf
Frog <84081437+huiji-park@users.noreply.github.com>
2023-12-26 12:47:26 +0900

    #55 안쓰는 querydsl 삭제

--- Server/src/main/java/phoenix/AM_PM/domain/chat/repository/ChatRepository.java
+++ Server/src/main/java/phoenix/AM_PM/domain/chat/repository/ChatRepository.java
@@ -1,19 +1,14 @@
 package phoenix.AM_PM.domain.chat.repository;
 
 import jakarta.persistence.LockModeType;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
 import phoenix.AM_PM.domain.chat.entity.Chat;
-import phoenix.AM_PM.domain.chat.repository.querydsl.ChatRepositoryCustom;
 
 import java.util.List;
 
 
-public interface ChatRepository extends JpaRepository<Chat, Integer>, ChatRepositoryCustom {
-    // 메시지 읽기 project_id 기반
-    Slice<Chat> findByProjectIdOrderByCreatedDateDesc(Integer projectId, Pageable pageable);
+public interface ChatRepository extends JpaRepository<Chat, Integer> {
     @Lock(LockModeType.PESSIMISTIC_WRITE)
     List<Chat> findByProjectIdOrderByCreatedDate(Integer projectId);
 }
