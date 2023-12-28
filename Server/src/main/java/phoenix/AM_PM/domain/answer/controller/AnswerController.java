22840603b59574a3b4ae4c643c96ad8407d2163a
Frog <84081437+huiji-park@users.noreply.github.com>
2023-12-28 09:32:42 +0900

    Update AnswerController.java

--- Server/src/main/java/phoenix/AM_PM/domain/answer/controller/AnswerController.java
+++ Server/src/main/java/phoenix/AM_PM/domain/answer/controller/AnswerController.java
@@ -15,9 +15,6 @@ import phoenix.AM_PM.domain.question.service.QuestionService;
 
 import java.util.List;
 
-import static phoenix.AM_PM.domain.answer.entity.QAnswer.answer;
-
-
 @Slf4j
 @RestController //HTTP Response Body에 객체 데이터를 JSON형식으로 반환하는 컨트롤러
 @RequiredArgsConstructor
